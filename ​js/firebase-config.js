9dce20f207502ac567a7580e6d1ef951c565c462
mff55951-oss <mff55951@gmail.com>
2026-03-25 18:57:34 +0600

    Update firebase-config.js

--- ​js/firebase-config.js
+++ ​js/firebase-config.js
@@ -1,1 +1,10 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBR8FMbjRjSL34-usa3xKSnJbhBOLda0uM",
+  authDomain: "poor-ai.firebaseapp.com",
+  projectId: "poor-ai",
+  storageBucket: "poor-ai.firebasestorage.app",
+  messagingSenderId: "655743636685",
+  appId: "1:655743636685:web:606f526d9430414c068584",
+  measurementId: "G-79Z3RV9JC9"
+};
